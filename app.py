67df3574b0b605b124bff1457670cea30a996e50
Dean <dtobrien23@gmail.com>
2023-03-06 17:38:41 +0000

    Connect to DB and display stations info as dictionary

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
+import functools
+from sqlalchemy import create_engine, text
+import traceback
 import dbinfo
 
 app = Flask(__name__)
@@ -6,6 +9,22 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     return render_template('index.html', gmkey=dbinfo.GMKEY)
+
+
+@app.route("/stations")
+@functools.lru_cache(maxsize=128)
+def get_stations():
+    engine = create_engine("mysql+mysqldb://{}:{}@{}:{}/{}".format(dbinfo.USER, dbinfo.PASSWORD, dbinfo.DB_URI, dbinfo.PORT, dbinfo.DB_NAME), echo=True)
+    sql = "select * from station;"
+    try:
+        with engine.connect() as conn:
+            rows = conn.execute(text(sql)).fetchall()
+            print('#found {} stations', len(rows), rows)
+            return jsonify([row._asdict() for row in rows])
+    except:
+        print(traceback.format_exc())
+        return "error in get_stations", 404
+
 
 if __name__ == "__main__":
     app.run(debug=True)
