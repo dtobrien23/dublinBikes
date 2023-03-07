7d943a507b4570fa11c60f9bf9ccc5b56b99870a
Dean <dtobrien23@gmail.com>
2023-03-07 12:53:09 +0000

    Added station map markers

--- static/js/index.js
+++ static/js/index.js
@@ -1,10 +1,34 @@
 let map;
 
+function addMarkers(stations) {
+  stations.forEach(station => {
+    var marker = new google.maps.Marker({
+      position: {
+        lat: station.position_lat,
+        lng: station.position_lng,
+      },
+      map: map,
+      title: station.name,
+      station_number: station.number,
+    });
+  });
+}
+
+function getStations() {
+  fetch("/stations")
+    .then((response) => response.json())
+    .then((data) => {
+      console.log("fetch response", typeof data);
+      addMarkers(data);
+    });
+  }
+
 function initMap() {
   map = new google.maps.Map(document.getElementById("map"), {
     center: { lat: 53.349811, lng: -6.260259 },
     zoom: 14,
   });
+  getStations();
 }
 
 window.initMap = initMap;
