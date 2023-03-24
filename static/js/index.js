d11fd735d09f71db9092b4139ad10f6ef118562d
Dean <dtobrien23@gmail.com>
2023-03-24 18:04:34 +0000

    tweaked to be up to date with dev branch

--- static/js/index.js
+++ static/js/index.js
@@ -1,5 +1,5 @@
 let map;
-let openStationInfo;  // used to make sure only one info window is open at a time 
+let openStationWindow;  // used to make sure only one info window is open at a time 
 
 function addMarkers(stations) {
   // add a marker to the map for each station
@@ -14,24 +14,24 @@ function addMarkers(stations) {
       station_number: station.number,
     });
     // create an info window for each marker that will open when clicked
-    const stationInfo = new google.maps.InfoWindow({
+    const stationWindow = new google.maps.InfoWindow({
       content: `<div style="color: black;"><h1>${station.name}</h1><p>Some information about the station</p></div>`,
     });
     // click event to open info window
     marker.addListener('click', () => {
       // any currently open window will close when another marker is clicked
-      if (openStationInfo) {
-        openStationInfo.close();
+      if (openStationWindow) {
+        openStationWindow.close();
       }
-      stationInfo.open(map, marker);
-      openStationInfo = stationInfo;
+      stationWindow.open(map, marker);
+      openStationWindow = stationWindow;
     });
   });
   // any currently open window will close when map is clicked
   map.addListener('click', () => {
-    if (openStationInfo) {
-      openStationInfo.close();
-      openStationInfo = null;
+    if (openStationWindow) {
+      openStationWindow.close();
+      openStationWindow = null;
     }
   });
 }
@@ -60,3 +60,18 @@ function initMap() {
 
 
 window.initMap = initMap;
+
+function stationsInfo(stations) {
+  let station_locations = ""
+  stations.forEach(station => {
+    station_locations += "<option value ='" + station.address + "'>"
+  });
+  document.getElementById('stations').innerHTML = station_locations;
+}
+
+function showRoute() {
+  let start_location = document.getElementById("start_location").value
+  let destination = document.getElementById("destination").value
+  let date = document.getElementById("date").value
+  let time = document.getElementById("time").value
+}
