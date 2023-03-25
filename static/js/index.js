e2e9df6ea140dc397446b4311933f84b68a1c9c7
Dean <dtobrien23@gmail.com>
2023-03-25 17:34:01 +0000

    updated markers and station functions to display availability data in info windows

--- static/js/index.js
+++ static/js/index.js
@@ -1,7 +1,9 @@
 let map;
-let openStationWindow;  // used to make sure only one info window is open at a time 
+let currentLocationWindow; // used to show the user where they are currently on the map
+let stationWindow;  // used to create info window for each marker
+let openStationWindow;  // used to make sure only one info window is open at a time
 
-function addMarkers(stations) {
+function addMarkers(stations, availability) {
   // add a marker to the map for each station
   stations.forEach(station => {
     const marker = new google.maps.Marker({
@@ -14,17 +16,26 @@ function addMarkers(stations) {
       station_number: station.number,
     });
     // create an info window for each marker that will open when clicked
-    const stationWindow = new google.maps.InfoWindow({
-      content: `<div style="color: black;"><h1>${station.name}</h1><p>Some information about the station</p></div>`,
-    });
+    availability.forEach(thisStation => {
+      if (thisStation.number == station.number) {  // to get the relevant availability for this station
+        let banking = "No";  // for card payment info
+        if (station.banking == 1) {
+          banking = "Yes";
+        };
+        marker.stationWindow = new google.maps.InfoWindow({
+        content: `<div style="color: black;"><h1>${station.name}</h1><p>Status: ${thisStation.status}<br>Available bikes: ${thisStation.available_bikes}<br>
+                  Available stands: ${thisStation.available_bike_stands}<br>Card payment: ${banking}</p></div>`,
+        });
+      }
+    }); 
     // click event to open info window
     marker.addListener('click', () => {
       // any currently open window will close when another marker is clicked
       if (openStationWindow) {
         openStationWindow.close();
       }
-      stationWindow.open(map, marker);
-      openStationWindow = stationWindow;
+      marker.stationWindow.open(map, marker);
+      openStationWindow = marker.stationWindow;
     });
   });
   // any currently open window will close when map is clicked
@@ -36,16 +47,20 @@ function addMarkers(stations) {
   });
 }
 
-
 // used to get data for map markers
-function getStations() {
+function getStationInformation() {
   fetch("/stations")
     .then((response) => response.json())
-    .then((data) => {
-      console.log("fetch response", typeof data);
-      addMarkers(data);
-      stationsInfo(data)
-    });
+    .then((stationData) => {
+      console.log("fetch response", typeof stationData);
+      stationsInfo(stationData)
+      fetch("/availability")
+        .then((response) => response.json())
+        .then((availabilityData) => {
+          console.log("fetch response", typeof availabilityData);
+          addMarkers(stationData, availabilityData);
+          });
+      });
   }
 
 
@@ -57,12 +72,56 @@ function initMap() {
   const bikeLayer = new google.maps.BicyclingLayer();
 
   bikeLayer.setMap(map);
-  getStations();
+  getStationInformation();
+
+  // creates an info window that shows the user there current location
+  currentLocationWindow = new google.maps.InfoWindow({
+      content: `<div style="color: black;"><p>Current Location</p></div>`,
+    });
+  const locationButton = document.createElement("button");
+
+  locationButton.textContent = "Pan to Current Location";
+  locationButton.classList.add("custom-map-control-button");
+  map.controls[google.maps.ControlPosition.TOP_CENTER].push(locationButton);
+  locationButton.addEventListener("click", () => {
+    // Try HTML5 geolocation.
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(
+        (position) => {
+          const pos = {
+            lat: position.coords.latitude,
+            lng: position.coords.longitude,
+          };
+
+          currentLocationWindow.setPosition(pos);
+          currentLocationWindow.open(map);
+          map.center(pos);
+        },
+        () => {
+          handleLocationError(true, currentLocationWindow, map.getCenter());
+        }
+      );
+    } else {
+      // Browser doesn't support Geolocation
+      handleLocationError(false, currentLocationWindow, map.getCenter());
+    }
+  });
 }
 
+// runs if the user doesn't allow their location to be shared
+function handleLocationError(browserHasGeolocation, currentLocationWindow, pos) {
+  currentLocationWindow.setPosition(pos);
+  currentLocationWindow.setContent(
+    browserHasGeolocation
+      ? "Error: The Geolocation service failed."
+      : "Error: Your browser doesn't support geolocation."
+  );
+  currentLocationWindow.open(map);
+}
 
 window.initMap = initMap;
 
+// lists all the stations in the input fields
 function stationsInfo(stations) {
   let station_locations = ""
   stations.forEach(station => {
