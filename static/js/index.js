cd3a41ae070f149992f09398f1644ca19bad2b27
MichaelMoyles <93867352+MichaelMoyles@users.noreply.github.com>
2023-03-30 09:02:16 +0100

    current location & google autoform added

--- static/js/index.js
+++ static/js/index.js
@@ -31,18 +31,18 @@ function addMarkers(stations, availability) {
           marker.setIcon(greenStation);
         } else {
           marker.setIcon(redStation);
-        };
+        }
 
         // create an info window for each marker that will open when clicked
         let banking = "No";  // for card payment info
         if (station.banking == 1) {
           banking = "Yes";
-        };
+        }
         marker.stationWindow = new google.maps.InfoWindow({
         content: `<div style="color: black;"><h1>${station.name}</h1><p>Status: ${thisStation.status}<br>Available bikes: ${thisStation.available_bikes}<br>
                   Available stands: ${thisStation.available_bike_stands}<br>Card payment: ${banking}</p></div>`,
         });
-      };
+      }
     });
 
     // click event to open info window
@@ -61,7 +61,7 @@ function addMarkers(stations, availability) {
     if (openStationWindow) {
       openStationWindow.close();
       openStationWindow = null;
-    };
+    }
   });
 }
 
@@ -71,7 +71,6 @@ function getStationInformation() {
     .then((response) => response.json())
     .then((stationData) => {
       console.log("fetch response", typeof stationData);
-      stationsInfo(stationData)
       fetch("/availability")
         .then((response) => response.json())
         .then((availabilityData) => {
@@ -90,122 +89,36 @@ function initMap() {
 
   bikeLayer.setMap(map);
   getStationInformation();
-  display_current_location(map);
+  //display_current_location(map);
 
-  const card = document.getElementById("pac-card");
-  const input = document.getElementById("pac-input");
-  const biasInputElement = document.getElementById("use-location-bias");
-  const strictBoundsInputElement = document.getElementById("use-strict-bounds");
-  const options = {
-    fields: ["formatted_address", "geometry", "name"],
-    strictBounds: false,
-    types: ["establishment"],
-  };
+  google.maps.event.addDomListener(window, 'load', autocompletePlaces)
 
-  map.controls[google.maps.ControlPosition.TOP_LEFT].push(card);
+}
+// Function to autocomplete list, currently commented out so as not to go over Google Maps API request limit
+function autocompletePlaces(callback) {
+  const input = document.getElementById("destination");
+  const autocomplete = new google.maps.places.Autocomplete(input);
 
-  const autocomplete = new google.maps.places.Autocomplete(input, options);
-
-  // Bind the map's bounds (viewport) property to the autocomplete object,
-  // so that the autocomplete requests use the current map bounds for the
-  // bounds option in the request.
-  autocomplete.bindTo("bounds", map);
-
-  const infowindow = new google.maps.InfoWindow();
-  const infowindowContent = document.getElementById("infowindow-content");
-
-  infowindow.setContent(infowindowContent);
-
-  const marker = new google.maps.Marker({
-    map,
-    anchorPoint: new google.maps.Point(0, -29),
-  });
-
-  autocomplete.addListener("place_changed", () => {
-    infowindow.close();
-    marker.setVisible(false);
-
+  autocomplete.addListener('place_changed', function () {
     const place = autocomplete.getPlace();
-
-    if (!place.geometry || !place.geometry.location) {
-      // User entered the name of a Place that was not suggested and
-      // pressed the Enter key, or the Place Details request failed.
-      window.alert("No details available for input: '" + place.name + "'");
-      return;
-    }
-
-    // If the place has a geometry, then present it on a map.
-    if (place.geometry.viewport) {
-      map.fitBounds(place.geometry.viewport);
-    } else {
-      map.setCenter(place.geometry.location);
-      map.setZoom(17);
-    }
-
-    marker.setPosition(place.geometry.location);
-    marker.setVisible(true);
-    infowindowContent.children["place-name"].textContent = place.name;
-    infowindowContent.children["place-address"].textContent =
-      place.formatted_address;
-    infowindow.open(map, marker);
-  });
-
-  // Sets a listener on a radio button to change the filter type on Places
-  // Autocomplete.
-  function setupClickListener(id, types) {
-    const radioButton = document.getElementById(id);
-
-    radioButton.addEventListener("click", () => {
-      autocomplete.setTypes(types);
-      input.value = "";
-    });
-  }
-
-  setupClickListener("changetype-all", []);
-  setupClickListener("changetype-address", ["address"]);
-  setupClickListener("changetype-establishment", ["establishment"]);
-  setupClickListener("changetype-geocode", ["geocode"]);
-  setupClickListener("changetype-cities", ["(cities)"]);
-  setupClickListener("changetype-regions", ["(regions)"]);
-  biasInputElement.addEventListener("change", () => {
-    if (biasInputElement.checked) {
-      autocomplete.bindTo("bounds", map);
-    } else {
-      // User wants to turn off location bias, so three things need to happen:
-      // 1. Unbind from map
-      // 2. Reset the bounds to whole world
-      // 3. Uncheck the strict bounds checkbox UI (which also disables strict bounds)
-      autocomplete.unbind("bounds");
-      autocomplete.setBounds({ east: 180, west: -180, north: 90, south: -90 });
-      strictBoundsInputElement.checked = biasInputElement.checked;
-    }
-
-    input.value = "";
-  });
-  strictBoundsInputElement.addEventListener("change", () => {
-    autocomplete.setOptions({
-      strictBounds: strictBoundsInputElement.checked,
-    });
-    if (strictBoundsInputElement.checked) {
-      biasInputElement.checked = strictBoundsInputElement.checked;
-      autocomplete.bindTo("bounds", map);
-    }
-
-    input.value = "";
   });
 }
 
+// Currently disabled
 // displays the current location of the user
 function display_current_location(map) {
   // creates an info window that shows the user there current location
   currentLocationWindow = new google.maps.InfoWindow({
       content: `<div style="color: black;"><p>Current Location</p></div>`,
     });
-  const locationButton = document.createElement("button");
 
-  locationButton.textContent = "Pan to Current Location";
+
+  const locationButton = document.getElementById('stations');
+
+  locationButton.innerHTMl = "<option value ='Current Location'>";
   locationButton.classList.add("custom-map-control-button");
   map.controls[google.maps.ControlPosition.TOP_CENTER].push(locationButton);
+
   locationButton.addEventListener("click", () => {
     // Try HTML5 geolocation.
     if (navigator.geolocation) {
@@ -242,21 +155,10 @@ function handleLocationError(browserHasGeolocation, currentLocationWindow, pos) 
   currentLocationWindow.open(map);
 }
 
-window.initMap = initMap;
-
-// lists all the stations in the input fields
-function stationsInfo(stations) {
-  let station_locations = ""
-  stations.forEach(station => {
-    station_locations += "<option value ='Current Location'>"
-    station_locations += "<option value ='" + station.address + "'>"
-  });
-  document.getElementById('stations').innerHTML = station_locations;
-}
-
 function showRoute() {
-  let start_location = document.getElementById("start_location").value
-  let destination = document.getElementById("destination").value
-  let date = document.getElementById("date").value
-  let time = document.getElementById("time").value
+  let x = 5
+  console.log(x)
 }
+
+window.initMap = initMap;
+console.log("destination");
